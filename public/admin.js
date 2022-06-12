939712ff3b5ab241c55632c1438ea1653eef0559
Thomas Crowe <wcrowe@g.clemson.edu>
2022-06-12 14:18:11 -0400

    Worked through activity.

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,71 @@
+//Fetch list of books.
 
-// Your Code Here
+let main = async () => {
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+    books.forEach(showBooks)
+
+}
+
+    let showBooks = (book) => {
+        let rootDiv = document.querySelector('#root')
+        
+        let list = document.createElement('li')
+        list.textContent = book.title
+        list.style.margin = '5px'
+
+        let numberInput = document.createElement('input')
+        numberInput.value = book.quantity
+        numberInput.style.margin = '5px'
+
+        let save = document.createElement('button') 
+        save.textContent = 'Save'
+        save.style.margin = '5px'
+
+        let deleteBook = document.createElement('button')
+        deleteBook.textContent = 'Delete Book'
+        deleteBook.style.marginLeft = '20px'
+
+        save.addEventListener('click', () => {
+            fetch('http://localhost:3001/updateBook', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: book.id,
+                    quantity: numberInput.value
+                })
+            })
+        })
+
+        // deleteBook.addEventListener('click', () => {
+        //     fetch('http://localhost:3001/updateBook', {
+        //         method: 'DELETE',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify({
+
+        //         })
+        //     })
+        // })
+
+
+        list.append(numberInput, save, deleteBook)
+        rootDiv.append(list)
+
+    }
+    
+    let addNewBook = document.createElement('button')
+    addNewBook.textContent = 'Add New Book'
+    addNewBook.style.margin = '20px'
+    addbtn.append(addNewBook)
+    
+    // addNewBook.addEventListener('click', () => {
+
+    // })
+    
+
+main()
+
